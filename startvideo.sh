6491a88491e0b6d9a0188fe6732be6e45555e345
Derekcat <derekcat@gmail.com>
2015-08-17 13:12:34 -0700

    Update startvideo.sh
    My rewrite which allows live updating of the contents of the videos folder, along with ridiculously thorough comments.

--- startvideo.sh
+++ startvideo.sh
@@ -1,39 +1,44 @@
 #!/bin/bash
+# Bash script by Tim Schwartz, http://www.timschwartz.org/raspberry-pi-video-looper/ 2013
+# Comments, clean up, improvements by Derek DeMoss, for Dark Horse Comics, Inc. 2015
 
-declare -A vids
+declare -A VIDS # make variable VIDS an Array
 
-FILES=~/video/
-current=0
-for f in `ls $FILES | grep .mp`
+FILES=~/video/ # A variable of this folder
+CURRENT=0 # Number of videos in the folder
+SERVICE='omxplayer' # The program to play the videos
+PLAYING=0 # Video that is currently playing
+
+getvids () # Since I want this to run in a loop, it should be a function
+{
+unset VIDS # Empty the VIDS array
+CURRENT=0 # Reinitializes the video count
+
+for f in `ls $FILES | grep .mp` # Step through the file in ~/video/
 do
-	vids[$current]=$f
-	let current+=1
-	echo ${vids[$current]}
+	VIDS[$CURRENT]=$f # add the filename found above to the VIDS array
+	let CURRENT+=1 # increment the video count
+#	echo ${VIDS[$CURRENT]} # Print the array element we just added
 done
-max=$current
-current=0
-echo "max = $max"
+}
 
-SERVICE='omxplayer'
+
 while true; do
-if ps ax | grep -v grep | grep $SERVICE > /dev/null
+if ps ax | grep -v grep | grep $SERVICE > /dev/null # Search for service, print to null
 then
-echo 'running'
-#sleep 1
+	echo 'running'
 else
-let current+=1
-if [ $current -ge $max ]
-then
-current=0
-fi
+	getvids # Get a list of the current videos in the folder
+	let PLAYING+=1
+	if [ $PLAYING -ge $CURRENT ] # if PLAYING is greater than or equal to CURRENT
+	then
+		PLAYING=0 # Reset to 0 so we play the "first" video
+	fi
 
-#FILE=/home/pi/video/`ls /home/pi/video/ | head -n 1`
-/usr/bin/omxplayer -r -o hdmi $FILES${vids[$current]}
-
+	/usr/bin/omxplayer -r -o hdmi $FILES${VIDS[$PLAYING]} # Play video 
+#	echo "Array size= $CURRENT" # error checking code
 fi
 done
-
-
 
 
 
